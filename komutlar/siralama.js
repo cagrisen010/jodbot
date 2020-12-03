7b06fa9f9f9165eb4f475f7454ddf448c797881e
Glitch (successful-parallel-turkey) <none>
2020-12-03 08:34:58 +0000

    🐑🏉 Checkpoint ./komutlar/siralama.js:23426179/247 ./komutlar/beg.js:23426179/425

--- komutlar/siralama.js
+++ komutlar/siralama.js
@@ -3,11 +3,11 @@ const { MessageEmbed } = require("discord.js");
 exports.execute = async (client, message, args) => {
    
     let leaderboard = client.eco.leaderboard({ limit: 15, raw: false });
-    if (!leaderboard || leaderboard.length < 1) return message.channel.send("❌ | Empty Leaderboard!");
+    if (!leaderboard || leaderboard.length < 1) return message.channel.send("❌ | Kimsede para yok?");
     const embed = new MessageEmbed()
-        .setAuthor(`Leaderboard of ${message.guild.name}!`, message.guild.iconURL)
+        .setAuthor(`${message.guild.name} sunucusu para sıralaması!`, message.guild.iconURL)
         .setColor("RANDOM")
-        .setThumbnail(client.users.cache.get(leaderboard[0].id) ? client.users.cache.get(leaderboard[0].id).displayAvatarURL : "https://cdn.discordapp.com/avatars/603948445362946084/a_f61398e073d78ae104e32b0517c891c3.gif")
+        .setThumbnail(client.users.cache.get(leaderboard[0].id) ? client.users.cache.get(leaderboard[0].id).displayAvatarURL : "")
         .setTimestamp();
     leaderboard.forEach(u => {
         embed.addField(`${u.position}. ${client.users.cache.get(u.id) ? client.users.cache.get(u.id).tag : "Unknown#0000"}`, `${u.money} 💸`);
@@ -17,6 +17,6 @@ exports.execute = async (client, message, args) => {
 
 exports.help = {
     name: "lb",
-    aliases: ["leaderboard"],
+    aliases: ["siralama","sıralama"],
     usage: `lb`
 }
