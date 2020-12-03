d1e9a973766b52566d43d6a72b54c68eaeb4fb79
Glitch (successful-parallel-turkey) <none>
2020-12-03 12:01:26 +0000

    🏘🌜 Checkpoint ./komutlar/casino.js:25342782/2601

---
+++ komutlar/casino.js
@@ -0,0 +1,22 @@
+exports.execute = async (client, message, args) => {
+
+
+  let amount = args[0]
+let chance = Math.floor(Math.random() * 2)
+if(chance === 1) {
+message.reply(`You won`)
+        let beg = await client.eco.beg(client.ecoDelUser, amount, { canLose: true, cooldown: 12000, customName: "search" });
+} else {
+message.reply(`You lost`)
+}
+  let winCash = parseInt(amount) * 2 //Y o u r c h o i c e
+
+        let beg = await client.eco.beg(client.ecoAddUser, winCash, { canLose: true, cooldown: 12000, customName: "search" });
+
+  };
+  
+exports.help = {
+    name: "casino",
+    aliases: ['casino'],
+    usage: "casino"
+}
