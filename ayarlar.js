89cfb25ff2835ea18c56876e77b4e008fefa14e2
Glitch (successful-parallel-turkey) <none>
2020-12-05 12:58:11 +0000

    👨‍❤️‍👨💏 Checkpoint ./ayarlar.js:25342782/96

--- ayarlar.js
+++ ayarlar.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: ".XxHQTQ.mKXqRJE43j-56ll-BxqDL3sV7pM",
+    token: "NzQ3NzM4NDU5MTA3NjIyOTUy.X0TPtA.awcFB3OsXe2ZN21XWu2_FndPtY4",
     prefix: "e!",
     admins: [
         "636573504598442084",
